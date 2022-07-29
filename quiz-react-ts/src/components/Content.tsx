5ae0eab598988d69209191925128092a6eed5af6
Bartosz Wiecheć <32521016+bwiechec@users.noreply.github.com>
2022-07-29 19:58:02 +0200

    quiz init

--- quiz-react-ts/src/components/Content.tsx
+++ quiz-react-ts/src/components/Content.tsx
@@ -5,26 +5,29 @@ import * as Icons from '@mui/icons-material';
 
 export default function Content() {
   return (
-    <Stack
-      style={{marginTop: '10%'}}
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      spacing={5}>
-      <Button
-        variant="contained"
-        size="large"
-      >
-        <Icons.Quiz />
-        <p> Start new quiz </p>
-      </Button>
-      <Button
-        variant="contained"
-        size="large"
-      >
-        <Icons.Add />
-        <p>Add new question</p>
-      </Button>
-    </Stack>
+    // <Stack
+    //   style={{marginTop: '10%'}}
+    //   direction="column"
+    //   justifyContent="center"
+    //   alignItems="center"
+    //   spacing={5}>
+    //   <Button
+    //     variant="contained"
+    //     size="large"
+    //   >
+    //     <Icons.Quiz />
+    //     <p> Start new quiz </p>
+    //   </Button>
+    //   <Button
+    //     variant="contained"
+    //     size="large"
+    //   >
+    //     <Icons.Add />
+    //     <p>Add new question</p>
+    //   </Button>
+    // </Stack>
+    <div>
+      test
+    </div>
   );
 }
--- quiz-react-ts/src/components/navbar.tsx
+++ quiz-react-ts/src/components/navbar.tsx
@@ -5,7 +5,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
-export default function navbar(){
+export default function Navbar(){
 
   return (
     <Box sx={{ flexGrow: 1 }}>
